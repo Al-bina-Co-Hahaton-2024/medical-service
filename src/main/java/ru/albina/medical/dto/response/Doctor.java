9d67cd2746b5c141480450de19cc1e53263320d3
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-06 22:02:21 +0300

    Added

--- src/main/java/ru/albina/medical/dto/response/Doctor.java
+++ src/main/java/ru/albina/medical/dto/response/Doctor.java
@@ -3,6 +3,7 @@ package ru.albina.medical.dto.response;
 import lombok.Data;
 import ru.albina.medical.domain.Modality;
 
+import java.time.LocalDate;
 import java.util.Set;
 import java.util.UUID;
 
@@ -10,6 +11,14 @@ import java.util.UUID;
 public class Doctor {
 
     private UUID id;
+
+    private Double hours;
+
+    private LocalDate startContract;
+
+    private LocalDate endContract;
+
+    private Long serviceNumber;
 
     private Double rate;
 
