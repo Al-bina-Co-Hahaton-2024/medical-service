bdd63b8048da5904bfb121750baf35cd0455fb84
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-07 22:23:35 +0300

    Fix!!.

--- src/main/java/ru/albina/medical/dto/response/Performance.java
+++ src/main/java/ru/albina/medical/dto/response/Performance.java
@@ -1,10 +1,16 @@
 package ru.albina.medical.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.albina.medical.domain.Modality;
 import ru.albina.medical.dto.TypeModality;
 
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Performance {
 
     private Modality modality;
