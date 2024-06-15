275f3d2b05c0f6c1770fbb10417ee0573547759e
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-15 15:40:56 +0300

    Set performance to average.

--- src/main/java/ru/albina/medical/mapper/DoctorMapper.java
+++ src/main/java/ru/albina/medical/mapper/DoctorMapper.java
@@ -17,16 +17,16 @@ public interface DoctorMapper {
     Float PERFORMANCE_HOURS = 8f;
 
     List<Performance> PERFORMANCE_LIST = List.of(
-            Performance.builder().modality(Modality.KT).typeModality(TypeModality.DEFAULT).value(Math.round(15 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.KT).typeModality(TypeModality.U).value(Math.round(16 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.KT).typeModality(TypeModality.U2).value(Math.round(11 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.DEFAULT).value(Math.round(12 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.U).value(Math.round(15 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.U2).value(Math.round(10 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.RG).typeModality(TypeModality.DEFAULT).value(Math.round(49 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.FLG).typeModality(TypeModality.DEFAULT).value(Math.round(181 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.MMG).typeModality(TypeModality.DEFAULT).value(Math.round(49 / PERFORMANCE_HOURS) + 1).build(),
-            Performance.builder().modality(Modality.DENSITOMETER).typeModality(TypeModality.DEFAULT).value(Math.round(84 / PERFORMANCE_HOURS) + 1).build()
+            Performance.builder().modality(Modality.KT).typeModality(TypeModality.DEFAULT).value(Math.round(28 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.KT).typeModality(TypeModality.U).value(Math.round(17 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.KT).typeModality(TypeModality.U2).value(Math.round(12 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.DEFAULT).value(Math.round(21 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.U).value(Math.round(16 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.MRT).typeModality(TypeModality.U2).value(Math.round(10 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.RG).typeModality(TypeModality.DEFAULT).value(Math.round(87 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.FLG).typeModality(TypeModality.DEFAULT).value(Math.round(316 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.MMG).typeModality(TypeModality.DEFAULT).value(Math.round(87 / PERFORMANCE_HOURS)).build(),
+            Performance.builder().modality(Modality.DENSITOMETER).typeModality(TypeModality.DEFAULT).value(Math.round(147 / PERFORMANCE_HOURS)).build()
     );
 
     @Mapping(target = "performances", expression = "java(PERFORMANCE_LIST)")
